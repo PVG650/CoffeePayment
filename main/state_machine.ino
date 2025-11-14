fddad312743073d95c326839f9c4e4437cc809ad
Pavel <pavel.villacis@live.de>
2025-11-14 09:29:14 +0100

    fixed typecasting issues when loading

--- main/state_machine.ino
+++ main/state_machine.ino
@@ -178,9 +178,8 @@ void state4() {  // Aufladen
 }
 bool transitionS4S5() {
   if (ok_button && stateJump > 2000) {
-    ladebetrag = scaledValue;
-    db.writeCell(nutzerNummer, 2, saldo + ladebetrag);
-    saldo = saldo + ladebetrag;
+    saldo = saldo + (float)scaledValue;
+    db.writeCell(nutzerNummer, 2, String(saldo, 2));
     rotaryEncoder.setBoundaries(-1000, 1000, false);
     return true;
   }
